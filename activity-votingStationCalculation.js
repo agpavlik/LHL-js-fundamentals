0cab4aed9f391f38cbd487f576bdf4b495020d62
agpavlik <agpavlik@gmail.com>
2023-03-14 01:49:50 +0000

    js activity - Voting Station Calculation.js

---
+++ activity-votingStationCalculation.js
@@ -0,0 +1,21 @@
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+const chooseStations = function (stations) {
+  const goodStations = [];
+  
+  for (const station of stations){
+    const capacity = station[1];
+    if (capacity >= 20){
+      const type = station[2];
+      if (type === 'school' || type === 'community centre'){
+        goodStations.push(station[0]);
+      }
+    }
+  }
+  return goodStations;
+};
+console.log(chooseStations(stations));
