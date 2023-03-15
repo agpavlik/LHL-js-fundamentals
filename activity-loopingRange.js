ef0ac4453a1534b159264ed612b974acb5d09788
agpavlik <agpavlik@gmail.com>
2023-03-15 17:04:51 +0000

    js activity - Looping Range

---
+++ activity-loopingRange.js
@@ -0,0 +1,16 @@
+
+
+function range (start, end, step){
+  let array = [];
+  if (start === undefined || end === undefined || step === undefined || start > end || step <= 0){
+    return array;
+  }
+  for (let i = start; i <= end; i += step){
+    array.push(i);
+}   return array;
+}
+
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
