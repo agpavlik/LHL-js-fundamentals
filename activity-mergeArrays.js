ba61a1409ecb513134f3da93dce706ab62a884e5
agpavlik <agpavlik@gmail.com>
2023-03-16 04:52:25 +0000

    js activity - Merge Arrays

---
+++ activity-mergeArrays.js
@@ -0,0 +1,11 @@
+let merge = function (mass1, mass2){
+  let array1 = mass1;
+  let array2 = mass2;
+  let array3 = array1.concat(array2);
+  return array3.sort();
+  
+}
+
+console.log(merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]), "=?", [ 1, 2, 3, 4, 4, 5, 6 ]);
+console.log(merge([ 4 ], [ 2, 5, 8 ]), "=?", [ 2, 4, 5, 8 ]);
+console.log(merge([ 1, 2, 6 ], []), "=?", [ 1, 2, 6 ]);
