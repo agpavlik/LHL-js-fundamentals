1b5892ab8b822e3f275928f3074a2c6c273fa0cb
agpavlik <agpavlik@gmail.com>
2023-03-17 05:17:37 +0000

    js activity - Objects - We're Rooting For You!

---
+++ activity-objects.js
@@ -0,0 +1,35 @@
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness';
+
+const judgeVegetable = (vegetables, metric) => {
+
+  let currentMax = vegetables[0][metric];
+  let indexOfPerson = 0; 
+  vegetables.forEach( (element, index) => {
+    if(element[metric] >= currentMax){
+      currentMax = element[metric];
+      indexOfPerson = index;
+    }
+  });
+
+  return vegetables[indexOfPerson].submitter;
+};
+
+console.log(judgeVegetable(vegetables, metric));
